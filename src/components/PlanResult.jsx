a11d830011d617ec6a01aec7addaabef485f1044
Manuel Alabor <msites@msites.net>
2015-05-19 15:56:14 +0200

    showing icons for a plans result state

--- src/components/PlanResult.jsx
+++ src/components/PlanResult.jsx
@@ -1,10 +1,11 @@
 var React            = require('react');
+var PlanResultState  = require('./PlanResultState');
 
 var PlanResult = React.createClass({
     render() {
         return (
             <tr className="table__row">
-                <td className="table__cell">{ this.props.planResult.state }</td>
+                <td className="table__cell"><PlanResultState state={ this.props.planResult.state } /></td>
                 <td className="table__cell">{ this.props.planResult.plan.shortName }</td>
             </tr>
         );
